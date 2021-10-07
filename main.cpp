8289c11db6d9184ab597c292b18aa0fb3695b2b9
SenorKaii <89370178+SenorKaii@users.noreply.github.com>
2021-10-07 11:13:53 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    //DECLARING VARIABLES
+    int hours;
+    string date;
+    double entrytime, exittime, totalpayment;
+    char carnumber[100];
+
+    //GET INPUT WITH SPACING
+    cout << "\nABC PRIVATE PARKING\n\n";
+    cout << "Enter Your Car Number: ";
+    cin.getline(carnumber,sizeof(carnumber));
+
+    //SET 2 DECIMAL POINT BEHIND
+    cout.setf (ios::fixed);
+    cout.precision(2);
+
+    //SHOW TEXT FOR USER TO ENTER  VALUE OF DATE
+    cout << "Date Today: ";
+    cin >> date;
+
+    //SHOW TEXT FOR USER TO ENTER VALUE OF TIME(ENTER)
+    cout << "Time You Enter This Parking (12 hours system): ";
+    cin >> entrytime;
+
+    //SHOW TEXT FOR USER TO ENTER VALUE OF TIME(EXIT)
+    cout << "Time You Exit This Parking (12 hours system): ";
+    cin >> exittime;
+    cout << "\n\n\n";
+
+    //PROCESS TO CALCULATE TOTAL PAYMENT DEPENDIND ON THE HOUR
+    hours = exittime - entrytime;
+    if (hours > 1)
+    {
+        totalpayment = 2 + (hours - 1);
+    }
+    else
+    {
+        hours = (12 - entrytime) + exittime;
+        totalpayment = 2 + (hours - 1);
+    }
+
+    //DISPLAY RESULTS
+    cout << setw(35) << "ABC PRIVATE PARKING" << endl;
+    cout << "CAR NUMBER: " << carnumber << endl;
+    cout << "DATE: " << date << endl;
+    cout << "TIME (ENTER): " << entrytime;
+    cout << setw(30) << "TIME (EXIT): " << exittime << endl;
+    cout << "TOTAL PAYMENT: " << totalpayment << endl;
+    cout << setw(45) << "THANK YOU FOR USING OUR SERVICE\n\n\n";
+    return 0;
+}

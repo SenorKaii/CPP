05e4c0b75e4edd63d99354578c194beaa7771eea
SenorKaii <89370178+SenorKaii@users.noreply.github.com>
2021-10-07 11:05:34 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void displayname();
+int main()
+{
+    displayname();
+    return 0;
+}
+
+void displayname()
+{
+    for (int i=1; i<=5; i++)
+    {
+        cout << "khalis" << endl;
+    }
+}
